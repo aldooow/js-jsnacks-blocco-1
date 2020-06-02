0005c0a3958b7a305a05ebdc51601f79996ea73c
Aldo Cahuana <aldooow@icloud.com>
2020-06-02 22:27:40 +0200

    Completed

--- js/script.js
+++ js/script.js
@@ -1,52 +1,76 @@
-// JS_SNAKE-1
-//L’utente inserisce due parole in successione, con due prompt.
-// Il software stampa prima la parola più corta, poi la parola più lunga.
+// JSnack 1
+/*L’utente inserisce due numeri in successione, con due prompt.
+  Il software stampa il maggiore.*/
+
+var numeroUno = document.getElementById("js_numero1");
+var numeroDue = document.getElementById("js_numero2");
+
+var buttonUno = document.getElementById("js_button1");
+var risultatoUno = document.getElementById("js_risultato1");
+
+buttonUno.addEventListener("click",
+function() {
+  risultatoUno.innerHTML = `Sono due numeri uguali`;
+  if (numeroUno.value > numeroDue.value ) {
+    risultatoUno.innerHTML = `Di questi due numeri il numero ${numeroUno.value} è il maggiore`;
+  } else if (numeroUno.value < numeroDue.value ) {
+    risultatoUno.innerHTML = `Di questi due numeri il numero ${numeroDue.value} è il maggiore`;
+  }
+});
+
+// JSnack 2
+/*L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.*/
+
+var parolaUno = document.getElementById("js_parola1");
+var parolaDue = document.getElementById("js_parola2");
+
+var buttonDue = document.getElementById("js_button2");
+var risultatoDue = document.getElementById("js_risultato2");
+
+buttonDue.addEventListener("click", function(){
+  risultatoDue.innerHTML = `Le due parole hanno la stessa lunghezza`;
+  if (parolaUno.value.length > parolaDue.value.length) {
+    risultatoDue.innerHTML = `la parola ${parolaUno.value} è piu lunga della parola ${parolaDue.value}.`;
+  } else if (parolaUno.value.length < parolaDue.value.length) {
+    risultatoDue.innerHTML = `la parola ${parolaDue.value} è piu lunga della parola ${parolaUno.value}.`
+  }
+});
+
+// JSnack 3
+/*Il software deve chiedere per 5 volte all’utente di inserire un numero.
+  Il programma stampa la somma di tutti i numeri inseriti. Esegui questo
+  programma in due versioni, con il for e con il while.*/
+
+var numberOne = document.getElementById("js_number1");
+var numberTwo = document.getElementById("js_number2");
+var numberThree = document.getElementById("js_number3");
+var numberFour = document.getElementById("js_number4");
+var numberFive = document.getElementById("js_number5");
+var numbers = [numberOne.value, numberTwo.value, numberThree.value, numberFour.value, numberFive.value];
+var buttonTre = document.getElementById("js_button3");
+var risultatoTre = document.getElementById("js_risultato3");
 
 
-// Chiedere informazione al utente!!!
+buttonTre.addEventListener("click",
+function() {
+  var somma = 0;
+  for (var i = 0; i < numbers.length; i++) {
+  somma += numbers[i];
+  }
+  risultatoTre.innerHTML = somma;
+});
 
-// Chiedere di inserire la prima parola!
-var primaParola = prompt('Dimmi la prima parola');
-
-// Chiedere di inserire la seconda parola!
-var secondaParola = prompt('Dimmi la seconda parola');
-
-// Stampare la parola piu lunga!!
-var messaggio;
-//  Vedere quale è la parola piu lunga!!
-if( primaParola.length > secondaParola.length ) {
-  messaggio = 'la parola ' + secondaParola + ' e piu corta della parola ' + primaParola;
-} else if( primaParola.length < secondaParola.length ) {
-  messaggio = 'la parola ' +  primaParola + ' e piu corta della parola ' + secondaParola;
-} else {
-  messaggio = 'le due parole hanno la stessa lunghezza'
-}
-
-alert(messaggio);
-// END JS_SNAKE-1
-
-// JS_SNAKE-2
-// Il software deve chiedere per 5 volte all’utente di
-// inserire un numero. Il programma stampa la somma di
-// tutti i numeri inseriti. Esegui questo programma in
-// due versioni, con il for e con il while.
-var somma = 0;
-for(var i = 1; i <= 5; i++) {
-  var numero= parseInt(prompt('insiriesce un numero'));
-  somma = somma + numero;
-}
-alert('la somma è ' + somma);
-// END JS_SNAKE-2
-
-// JS_SNAKE-3
- // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
- // chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+// JSnack 4
+/*In un array sono contenuti i nomi degli invitati alla festa del
+  grande Gatsby, chiedi all’utente il suo nome e comunicagli se può
+  partecipare o no alla festa.*/
 
 var nomeForm = document.getElementById('js_nome-form');
 
 var listaFesta = ['Aldo', 'Beto', 'Ale', 'Maria', 'Paolo'];
 // Tasti Email!!!
-var buttonInviaUno = document.getElementById('js_button-invia-1');
+var buttonQuattro = document.getElementById('js_button4');
 var alert = document.getElementById('js_alert');
 
 
@@ -55,7 +79,7 @@ var nomeCorretto = false;
 // Elementi da nascondere!!!
 
 // Evento dopo il "Click" sul tasto "Invio"
-buttonInviaUno.addEventListener('click',
+buttonQuattro.addEventListener('click',
 function() {
   // Se inseriscono una mail della lista, autorizziamo l'acceso!!!
   for ( var i = 0; i < listaFesta.length; i++) {
@@ -72,4 +96,24 @@ function() {
   }
 
 })
-// END JS_SNAKE-3
+
+// JSnack 5
+/*Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte
+  le cifre che compongono il numero.*/
+
+var numberoQuattroCifre = document.getElementById('js_numero-quattro');
+
+
+var buttonCinque = document.getElementById('js_button5');
+var risultatoCinque = document.getElementById('js_risultato5');
+
+buttonCinque.addEventListener('click',
+function(){
+  var numberoQuattroCifreArray = numberoQuattroCifre.value.split('');
+  console.log(numberoQuattroCifreArray)
+  var somma2 = 0;
+  for(var i = 0; i < numberoQuattroCifreArray.length; i++) {
+    somma2 += parseInt(numberoQuattroCifreArray[i]);
+    risultatoCinque.innerHTML = somma2
+  }
+})
